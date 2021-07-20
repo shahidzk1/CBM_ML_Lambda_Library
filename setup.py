2a21d34b054f40c13313a0051098fb6e3174c147
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-07-20 16:44:45 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='ovas',
+  version='1.0',
+  description='Testing installation of Package',
+  url='https://github.com/CBM-ML/hydron-ml',
+  author='Ali Imdad Khan',
+  author_email='ali-imdad.khan@student.uni-tuebingen.de',
+  license='MIT',
+  packages=['ovas'],
+  install_requires=['numpy','uproot']
+)
+
