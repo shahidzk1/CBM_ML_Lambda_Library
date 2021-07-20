7132bbb5251a37ebf6c9a10cbe7f3f2e7652609c
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2021-07-20 17:26:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -51,16 +51,7 @@ from setuptools import setup, find_packages
 
 
         
-setup(
-  name='CBM_ML',
-  version='1.0',
-  description='First ML Python library',
-  author='Shahid Khan',
-  license='MIT',
-  packages=find_packages()
-)        
-        
-        
+       
 SETUP = Setup(
     name="CBM_ML",
 
